1e89f03c124f0b221eddad4f77ac7add153f9feb
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-06-30 18:52:25 +0600

    server updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -16,10 +17,34 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeaders = req.headers.authorization 
+  if(!authHeaders){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  const token = authHeaders.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(403).send({message: 'forbidden access'})
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
+
 async function run() {
   try {
     const serviceCollection = client.db("geniusCar").collection("services");
     const orderCollection = client.db("geniusCar").collection("orders");
+
+    // implementation of jwt token
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     app.get("/services", async (req, res) => {
       const query = {};
@@ -39,7 +64,11 @@ async function run() {
     // all the orders related api from ordersCollection db
 
     // get all the orders from an user from this api through query (?email=example@gamil.com)
-    app.get("/orders", async (req, res) => {
+    app.get("/orders", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      if(decoded.email !== req.query.email){
+        res.status(403).send({message: 'unauthorized access'})
+      }
       let query = {};
       if (req.query.email) {
         query = {
@@ -59,20 +88,20 @@ async function run() {
     });
 
     // api for updating doc
-    app.patch('/orders/:id', async (req, res) => {
+    app.patch("/orders/:id", async (req, res) => {
       const id = req.params.id;
       const status = req.body.status;
       const query = {
-          _id: new ObjectId(id)
+        _id: new ObjectId(id),
       };
       const updateDoc = {
-          $set: {
-              status: status
-          }
-      }
+        $set: {
+          status: status,
+        },
+      };
       const result = await orderCollection.updateOne(query, updateDoc);
       res.send(result);
-  })
+    });
 
     // api for deleting orders
     app.delete("/orders/:id", async (req, res) => {
